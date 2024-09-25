b3196dfd37e3b42437173b66f8bb9d7ead3b628b
CheaRitheavatey <2023447chea@aupp.edu.kh>
2024-09-25 21:25:25 +0200

    Update week_three.py

--- week_three.py
+++ week_three.py
@@ -179,3 +179,31 @@ for i in range(1,6):
     for j in range(1,i+1):
         print(j,end=" ")
     print()
+
+print()
+
+# homework 
+for i in range(1,6):
+    for j in range(i,6,1):
+        print(j,end=" ")
+    print()
+
+# output:
+# 1 2 3 4 5
+# 2 3 4 5
+# 3 4 5
+# 4 5
+# 5
+
+# homework 
+for i in range(5, 0, -1): 
+    for j in range(i, 0, -1):  
+        print(j, end=" ")  
+    print()
+
+# output:
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
