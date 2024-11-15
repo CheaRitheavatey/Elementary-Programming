6704cc0ef37b4f7978b669eb9138079d32070b4f
CheaRitheavatey <2023447chea@aupp.edu.kh>
2024-11-15 11:28:23 +0200

    update week 9

--- week_nine.py
+++ week_nine.py
@@ -55,5 +55,102 @@ def exercise1(path):
     # if tehy only want to remove just \n from the end then
     # x = i.replace('\n','')
 
-print(exercise1("file_handling.txt"))
+# print(exercise1("file_handling.txt"))
+
+
+# practice 2
+def exercise2():
+    list = ["apple", "banana", "cranberry", "durian"]
+
+    if not os.path.exists("file_handling.txt"):
+        afile = open("file_handling.txt","x")
+    else:
+        afile = open("file_handling.txt","w")
     
+    for i in list:
+        afile.write(i+'\n')
+    afile.close()    
+    afile = open("file_handling.txt")
+    print(afile.read())
+    afile.close()
+
+# exercise2()
+
+
+# practice 3
+# read a random line from a file and print it
+from random import choice
+def exercise3(filepath):
+    # another way to open a file is using keyword "with" "with" will open and close automatically but you have to define scope more carefully 
+    with open(filepath,'a') as myfile:
+        myfile.write("Orange")
+    
+    myfile = open(filepath)
+    list = myfile.readlines()
+    random = choice(list).strip()
+    myfile.close()
+    return random
+
+
+# print(exercise3("file_handling.txt"))
+
+# practice 4 (not file related)
+# ask user for name year of birth and country they are from make sure the year enter is in the correct format (4 digit numbers)
+# in case of an inccorect input give user enough information so they know what should they enter
+# and program keep asking until it gets the right format after getting the information welcome the user like: welcome name, you were born in year and from country
+
+def exercise4():
+    name = input("Enter Name: ")
+    country = input("Enter Country: ")
+
+    while True:
+        try:
+            year = input("Enter Year: ")
+            
+            if len(str(year)) != 4 or int(year) > 2024:
+                print("invalid input year")
+                continue
+            else:
+                year = int(year)
+                age = 2024 - year
+                print( f"Welcome {name}, You are {age} years old and from {country}")
+        except ValueError:
+            print("invalid input")
+            
+# print(exercise4())
+
+
+# practice 5
+# create a function to add user informaiton received from the previous function to the names.txt file if the user with the same year of birth and country doesnt not 
+# if the name.txt file couldn not be found create the file and add the user there
+# this shouldnt be case sensitive
+# keep the writing in the file as it is 
+# let the user know if they are added to the file or they were alrady listed
+def exercise5():
+    dict = {'name': '',
+            'year': '',
+            'country':''}
+    
+    if not os.path.exists("names.txt"):
+        file = open("names.txt","x")
+    else:
+        file = open("names.txt", "w")
+
+    for i in dict:    
+        file.write(i+' ')
+    file.close()
+
+    # append in the last function
+   
+    exercise4()
+    name = ""
+    year = 0
+    country = ""
+
+    user = f"{name} {year} {country}\n"
+    if user in exercise4():
+        file = open("names.txt","a")
+        file.write(user)
+        print("you are added to the list")
+
+exercise5()
