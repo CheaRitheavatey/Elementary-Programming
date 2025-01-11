a5e0ee7c12acc2881ff0ee8bdbb19cd9d9eda633
CheaRitheavatey <2023447chea@aupp.edu.kh>
2025-01-11 16:04:36 +0200

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -17,6 +17,8 @@ class leetcode {
         }
 
         return 0;
+
+        // time complexity is O(n) because of the for loop;
     }
     // search insert position
     // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
