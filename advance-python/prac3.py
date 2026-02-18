6266a0c038e759b8fd7efa48485ab12e71c787c1
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2026-02-18 16:43:52 +0100

    update class lessons

--- advance-python/prac3.py
+++ advance-python/prac3.py
@@ -105,3 +105,130 @@ def outer():
 
 f = outer()
 print(f())
+
+
+"""
+class lessons
+"""
+
+# nested function 
+
+# nested function procedural
+def greeting(name):
+    def inner():
+        print(f'hello {name}!')
+    inner()
+
+greeting('joe')
+
+
+# nested function that have return
+def greetings(name):
+    def inner():
+        return f'hello {name}'
+    return inner()
+
+print(greetings('joe'))
+
+
+def add(x):
+    def inner_add(y):
+        return x + y
+    return inner_add
+
+
+# higher order function: can return function and accept function
+
+# recursion
+def factor(n):
+    # edge case
+    if n <= 1:
+        return 1
+    
+    return n * factor(n-1)
+
+
+def fibonacci(n):
+    # edge case
+    if n <= 1:
+        return n
+    
+    return   fibonacci(n - 1) +fibonacci(n - 2)
+
+
+def print_recursion(func, n):
+    for i in range(1,n+1):
+        print(f"{i}: {func(i)}")
+        
+        
+print_recursion(factor, 3)
+print_recursion(fibonacci, 3)
+
+import random
+# list generator function
+# generate a list of random numbers with a given length
+# filter - even number only and odd number only
+# user can also define the random number range (value1, value2)
+def even_number(n):
+    if n % 2 == 0:
+        return n
+    
+
+def odd_number(n):
+    if n % 2 != 0:
+        return n
+
+def list_generator(n, func, value1, value2):
+    lst = []
+    while len(lst) < n:
+        number = random.randint(value1,value2)
+        val = func(number)
+        if val is not None:
+            lst.append(number)
+        
+        # if func(number):
+        #     lst.append(number)
+            
+    return lst
+
+print(list_generator(10, even_number,0,2 ))
+
+
+# lambda expression
+square = lambda x : x**2 # square is a function
+print(square(5))
+
+reverse = lambda x : x[::-1] # 3 s: start, stop, step
+print(reverse('apple'))
+
+
+even = lambda x : x % 2 ==0
+print(even(234))
+
+# lambda and sorting
+l = [10,25,-9,74,65,-50]
+print(sorted(l))
+
+s = ['B', 'c', 'D', 'a', 'F', 'x', 'S']
+print(sorted(s, key=lambda x: x.upper()))
+
+# sort by length
+words = ['apples', 'peach', 'lemon', 'strawberry']
+print(sorted(words, key=lambda x: len(x)))
+
+#  count words in a string
+my_string = 'this is an example. a short text for the task.'
+result = lambda x: len(x.split())
+print(result(my_string))
+
+
+# sort a dictionary
+d = {
+    'a': 30,
+    'x': 500,
+    'c': 200
+}
+
+print(sorted(d))
+# sort by value
+print(sorted(d, key=lambda x: d[x]))
