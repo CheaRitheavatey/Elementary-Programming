186457482c52a6584df5ad049d30629115bcd57c
CheaRitheavatey <ritheavateychea@gmail.com>
2026-04-29 15:37:39 +0200

    Update leetcode.py

--- advance-python/leetcode.py
+++ advance-python/leetcode.py
@@ -1,3 +1,61 @@
+# def encode(strs: list[str]) -> str:
+alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y','z']
+inputs = ["hello", "world", "apple"] # "hello\nworld\napple"
+inputss = ("\n".join(inputs))
+print(inputss)
+new_words = ""
+for i in inputss:
+    if i == "\n":
+        new_words = new_words.rstrip()
+        new_words+= "\n"
+        continue
+    num = (ord(str(i)))
+    new_words += str(num) + " "
+    print(f"{i} = {ord(str(i))}")
+    
+    # 10 = new words
+array = []
+print(new_words)
+print(chr(111))
+word = ""
+
+words = new_words.split("\n")
+print(new_words)
+print(words)
+for i in words:
+    print(i)
+    c = i.split(" ")
+    print(c)
+    for char in c:
+        if char == "":
+            continue
+        
+        word += (chr(int(char)))
+        print(word)
+    array.append(word)
+    word = ""
+print(array)
+def topKFrequent(nums: list[int], k: int) -> list[int]:
+        # k will be the len of the output arr
+        store = {}
+        result = []
+        x = 0
+        for i in nums:
+            store[i] = store.get(i,0) + 1
+        print(store)
+        
+        while x <= k-1:
+            for i in sorted(store.values())[::-1]:
+                result.append(i)
+            x+=1
+        return result
+        
+        
+        
+
+
+        
+print(topKFrequent([1,4,4,4,7,7],2))
 def exclusiveTime( n: int, logs: list[str]) -> list[int]:
     result = [0] * n
     stack = []
